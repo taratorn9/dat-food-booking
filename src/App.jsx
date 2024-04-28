fbfd510c0901c14e4895f73216c34690717664e6
taratorn9 <mrwoodpecker1995@gmail.com>
2024-04-28 18:31:24 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>DAT food booking</h1>
+      <div className="card">
+        <p>select language to start</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
